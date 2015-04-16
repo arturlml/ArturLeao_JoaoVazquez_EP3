78e6ee4ea43ed25ba8b619cb1498abaef69615c7
Joao Vazquez <joaovazquez96@gmail.com>
2015-04-16 14:47:15 -0300

    tentando criar uma lista para resolver o problema do arquivo usuário, pois dicionário não deu certo

--- dieta1.py
+++ dieta1.py
@@ -6,5 +6,28 @@ def TMB():
 print(TMB())
 
 def qtdecalorica():  #quantidade energetica em kcal necessitada 
-    return TMB()*1.725 #o numero 1.725 é porque o nivel de atividade do usuario é alto
-print(qtdecalorica())
+    return TMB()*1.725 #o numero que multiplica o tmb é 1.725 porque o nivel de atividade do usuario é alto
+print(qtdecalorica()) #só para saber qual a necessidade energetica, que é de 2833.416kcal
+
+arquivo_usuario = open("usuario.csv","r")
+leitura_usuario = arquivo_usuario.readlines()
+#print(leitura_usuario)
+lista=[]
+lista2=[]
+for i in leitura_usuario[3:]:
+    linha_usuario = i.split(',')
+    if i == 3:
+        lista1 = lista.append(linha_usuario[1:])
+    print(lista1)
+    else:
+        break    
+    
+
+
+arquivo_alimentos = open('alimentos.csv','r')
+leitura_alimentos = arquivo_alimentos.readlines()
+dict_alimentos = dict()
+for e in leitura_alimentos[1:]:
+    linha_alimentos = e.split(',')
+    dict_alimentos[linha_alimentos[0]] = linha_alimentos[1], linha_alimentos[2], linha_alimentos[3], linha_alimentos[4], linha_alimentos[5]
+#print(dict_alimentos)
