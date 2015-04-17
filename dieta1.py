f4614e258c04f1cfd62641998230ece8d4378925
Joao Vazquez <joaovazquez96@gmail.com>
2015-04-17 11:43:42 -0300

    corrigindo alguns erros estranhos que apareceram no ultimo commit

--- dieta1.py
+++ dieta1.py
@@ -3,31 +3,13 @@ def TMB():
     altura = 164
     idade = 30    
     return 88.36+(13.4*peso)+(4.8*altura)-(5.7*idade)
-<<<<<<< Updated upstream
-print(TMB() )#Nível basal do metabolismo
+
+print(TMB())  #Nível basal do metabolismo
 
 def qtdecalorica():  #quantidade energetica em kcal necessitada 
-    return TMB()*1.725 #o numero que multiplica o tmb é 1.725 porque o nivel de atividade do usuario é alto
-print(qtdecalorica()) #só para saber qual a necessidade energetica, que é de 2833.416kcal
-dict_usuario = dict()
-lista_consumido = []         
-datas = []          
-arquivo_usuario = open("usuario.csv","r")
-leitura_usuario = arquivo_usuario.readlines()
-print(leitura_usuario)
-for e in range(3,len(leitura_usuario)):
-    linha_alimento = leitura_usuario[e].split(',')
-    lista_consumido.append(linha_alimento)
-
-for b in lista_consumido:
-    if b[0] not in datas:
-        datas.append(b[0])
-=======
-#print(TMB())
-
-def qtdecalorica():  #quantidade energetica em kcal necessitada 
-    return TMB()*1.725 #o numero que multiplica o tmb é 1.725 porque o nivel de atividade do usuario é alto
-#print(qtdecalorica()) #só para saber qual a necessidade energetica, que é de 2833.416kcal
+    return TMB()*1.725  #o numero que multiplica o tmb é 1.725 porque o nivel de atividade do usuario é alto
+print(qtdecalorica())  #só para saber qual a necessidade energetica, que é de 2833.416kcal
+        
 
 ''' Criando um dicionário com as informaões do que o usuário comeu '''
 
@@ -45,25 +27,16 @@ for y in range(3,len(leitura_usuario)):  #comeca do 3 porque as informações qu
 for b in lista_consumido:   #adiciona as datas que o usuario quiser caso hajam mais dias de dieta
     if b[0] not in datas:
         datas.append(b[0]) #adiciona a lista de datas as datas presentes nas listas com as informações
->>>>>>> Stashed changes
         
 for a in datas:
     dict_usuario[a] = []
     for c in lista_consumido:
-<<<<<<< Updated upstream
-        if a == c[0]:      
-            dict_usuario[a].append([c[1],c[2]])
-=======
         if a == c[0]:                               
             dict_usuario[a].append([c[1],c[2]])  #se a chave do dic for igual ao primeiro termo da lista_consumido adicionar os dois proximos elementos da lista_consumida ao dicionário 
 
 print(dict_usuario)
 
-    
 ''' Criar um dicionário com as informações de todos os alimentos fornecidos '''
->>>>>>> Stashed changes
-
-print(dict_usuario)
 
 arquivo_alimentos = open('alimentos.csv','r')
 leitura_alimentos = arquivo_alimentos.readlines()
