1e13086c62f3cb01b34779d9fa5893d2ab2b5f13
Joao Vazquez <joaovazquez96@gmail.com>
2015-04-22 12:58:07 -0300

    graficos
    insercao dos graficos

--- dieta1.py
+++ dieta1.py
@@ -163,6 +163,8 @@ calorias_milk = (float(dict_usuario['07/04/15'][3][1]) * float(dict_alimentos['M
 gordura_milk = (float(dict_usuario['07/04/15'][3][1]) * float(dict_alimentos['MILK-SHAKE DE CHOCOLATE'][4]))/100
 #print(gordura_milk)
 
+'''CALCULO DE PROT, CARBO, CAL E GORDURA TOTAL DO DIA 6'''
+
 proteina_total6 = proteina_iogurte + proteina_suco + proteina_mamao + proteina_biscoito  #calcula o total de proteina ingerida no dia 6
 #print(proteina_total6)
 
@@ -170,9 +172,88 @@ carbo_total6 = carbo_iogurte + carbo_suco + carbo_mamao + carbo_biscoito
 #print(carbo_total6)
 
 calorias_total6 = calorias_iogurte + calorias_suco + calorias_mamao + calorias_biscoito
-print(calorias_total6)
+#print(calorias_total6)
 
 gordura_total6 = gordura_iogurte + gordura_suco + gordura_mamao + gordura_biscoito
 #print(gordura_total6)
+
+'''CALCULO DE PROT, CARBO, CAL E GORDURA TOTAL DO DIA 7'''
+
+proteina_total7 = proteina_creme + proteina_macarrao + proteina_nhoque + proteina_milk
+#print(proteina_total7)
+
+carbo_total7 = carbo_creme + carbo_macarrao + carbo_nhoque + carbo_milk
+#print(carbo_total7)
+
+calorias_total7 = calorias_creme + calorias_macarrao + calorias_nhoque + calorias_milk
+#print(calorias_total7)
+
+gordura_total7 = gordura_creme + gordura_macarrao + gordura_nhoque + gordura_milk
+#print(gordura_total7)
+
+caloria_total = [calorias_total6,calorias_total7]
+proteina_total = [proteina_total6,proteina_total7]
+carbo_total = [carbo_total6,carbo_total7]
+gordura_total = [gordura_total6,gordura_total7]
+T = [6,7]
+import matplotlib.pyplot as plt
+
+def MostraGraficoCalorias(T,caloria_total,qtdecalorica,yy):
+     if yy == 0:
+        plt.plot(T, qtdecalorica)
+        plt.plot(T, caloria_total)
+        plt.axis([0, max(T), 0, 2000])
+        plt.ylabel('Quantidade necessária')
+        plt.xlabel('Tempo (dias)')
+        plt.title('Calorias')
+        plt.show()  
+        return None
+     else:
+        fig, ax1 = plt.subplots()
+        ax1.plot(T, caloria_total, 'b.')
+        ax1.set_xlabel('Tempo (dias)')
+        # Make the y-axis label and tick labels match the line color.
+        ax1.set_ylabel('Calorias Ingeridas(kcal)', color='b')
+        for tl in ax1.get_yticklabels():
+            tl.set_color('b')
+        
+        ax2 = ax1.twinx()
+        ax2.plot(T, qtdecalorica, 'r.')
+        ax2.set_ylabel('Quantidade necessária(kcal)', color='r')
+        for tl in ax2.get_yticklabels():
+            tl.set_color('r')
+        plt.show() 
+        return None
+#MostraGraficoCalorias(T,caloria_total,2833.416,1)
+
+def MostraGraficoProteina(T,proteina_total):
+    fig, ax1 = plt.subplots()
+    ax1.plot(T, proteina_total, 'g.')
+    ax1.set_xlabel('Tempo(dias)')
+    ax1.set_ylabel('Proteína ingerida(g)', color='g')
+    for tl in ax1.get_yticklabels():
+        tl.set_color('g')
+    plt.show()
+MostraGraficoProteina(T,proteina_total)
+
+def MostraGraficoCarbo(T,carbo_total):
+    fig, ax1 = plt.subplots()
+    ax1.plot(T, carbo_total, 'g.')
+    ax1.set_xlabel('Tempo(dias)')
+    ax1.set_ylabel('Carboidrato ingerido(g)', color='g')
+    for tl in ax1.get_yticklabels():
+        tl.set_color('g')
+    plt.show()
+MostraGraficoCarbo(T,carbo_total)
+
+def MostraGraficoGordura(T,gordura_total):
+    fig, ax1 = plt.subplots()
+    ax1.plot(T, gordura_total, 'g.')
+    ax1.set_xlabel('Tempo(dias)')
+    ax1.set_ylabel('Gordura ingerida(g)', color='g')
+    for tl in ax1.get_yticklabels():
+        tl.set_color('g')
+    plt.show()
+MostraGraficoGordura(T,gordura_total)
 
 
